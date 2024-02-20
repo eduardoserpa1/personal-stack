a7bab4b777f1e93b8dde5eb46ef67f7d320d59b4
eduardoserpa1 <dudu.rserpa@gmail.com>
2024-02-20 14:50:02 -0300

    install react-router-dom, axios, styled components and add simple mvc structure

---
+++ personal-stack-front/src/routes/Home.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+
+const AppContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-image: linear-gradient(90deg, #002F52 35%, #326589);
+`
+
+function Home() {
+  return (
+    <AppContainer>
+      <p>THIS IS HOME ROUTE PAGE</p>
+    </AppContainer>
+  );
+}
+
+export default Home;
