e3fd97e7bf38493d48afa505b2730f7736ade2e5
eduardoserpa1 <dudu.rserpa@gmail.com>
2024-07-29 16:44:43 -0300

    add doc for projects and fix descriptions

--- src/routes/projects/GeneticAlgorithm.js
+++ src/routes/projects/GeneticAlgorithm.js
@@ -1,17 +1,61 @@
 import styled from 'styled-components'
 import ContentBlock from '../../components/Core/ContentBlock';
-import { SubTitle, TextParagraph, Title } from '../../components/Core/SingleStyledComponents';
+import { Code, SubTitle, TextParagraph, Title, TitleMediumBackward, TitleShort } from '../../components/Core/SingleStyledComponents';
 import Container from '../../components/Core/Container'
 
 function GeneticAlgorithm() {
   return (
     <Container>
      <ContentBlock>
-      <Title>genetic-algoritm</Title>
+      <Title>Algoritmo genético</Title>
       <SubTitle>Algoritmo genético desenvolvido em java para solucionar o problema do caixeiro viajante</SubTitle>
+      <TitleMediumBackward>Descrição do problema</TitleMediumBackward>
+      <TitleShort>Problema do Caixeiro Viajante</TitleShort>
+
       <TextParagraph>
-        
+      O Problema do Caixeiro Viajante (TSP - Traveling Salesman Problem) é um dos problemas mais clássicos e estudados na teoria da computação e otimização combinatória. Ele pode ser descrito da seguinte forma:
       </TextParagraph>
+
+      <TextParagraph>
+      Um vendedor (caixeiro viajante) precisa visitar um certo número de cidades, partindo de uma cidade inicial, visitando cada uma das outras cidades exatamente uma vez, e retornando à cidade de origem. O objetivo é encontrar a rota mais curta possível que permita ao caixeiro viajante visitar todas as cidades e retornar ao ponto de partida.
+      </TextParagraph>
+
+      <TextParagraph>
+      Matematicamente, o problema pode ser representado por um grafo completo onde os vértices representam as cidades e as arestas representam os caminhos entre as cidades, com um custo (ou distância) associado a cada aresta. A solução ideal do problema é a permutação das cidades que resulta no menor custo total de viagem.
+      </TextParagraph>
+
+      <TextParagraph>
+      O TSP é um problema NP-difícil, o que significa que não existe um algoritmo eficiente conhecido que possa resolver todos os casos do problema de forma rápida (em tempo polinomial). Devido à sua complexidade, várias abordagens heurísticas e metaheurísticas, como algoritmos genéticos, algoritmos de colônia de formigas e algoritmos de busca tabu, são frequentemente usadas para encontrar soluções aproximadas em tempo razoável.
+      </TextParagraph>
+
+      <TextParagraph>
+      No contexto do meu desenvolvimento, implementei um algoritmo genético em Java para abordar o TSP. O algoritmo genético é uma técnica inspirada na evolução natural, que utiliza processos como seleção, cruzamento e mutação para evoluir uma população de soluções candidatas ao longo de várias gerações, na tentativa de encontrar uma solução cada vez melhor para o problema do caixeiro viajante.
+      </TextParagraph>
+
+      <TitleMediumBackward>Como executar o código</TitleMediumBackward>
+
+      <TextParagraph>
+      Você deve compilar os arquivos Java (Main, City, Route) com a linha de comando abaixo no diretório /src:
+      </TextParagraph>
+      
+      <Code>javac *.java</Code>
+
+      <TextParagraph>
+      Depois, você pode executar o algoritmo de três maneiras diferentes:
+      </TextParagraph>
+
+      <ol>
+        <li>Executar a partir do arquivo de entrada do zero:</li>
+        <Code>{"java Main <caminho_do_arquivo>"}</Code>
+
+        <li>Executar a partir do arquivo de entrada do zero:</li>
+        <Code>{"java Main <caminho_do_arquivo> <caminho_do_arquivo_de_checkpoint>"}</Code>
+
+        <li>Executar a partir do arquivo de entrada do zero:</li>
+        <Code>{"java Main -h"}</Code>
+      </ol>
+
+      
      </ContentBlock>
     </Container>
   );
--- src/routes/projects/NqueensVariationProblem.js
+++ src/routes/projects/NqueensVariationProblem.js
@@ -1,15 +1,80 @@
 import styled from 'styled-components'
 import ContentBlock from '../../components/Core/ContentBlock';
-import { TextParagraph } from '../../components/Core/SingleStyledComponents';
+import { Code, SubTitle, TextParagraph, Title, TitleMediumBackward, TitleShort } from '../../components/Core/SingleStyledComponents';
 import Container from '../../components/Core/Container'
 
 function NqueensVariationProblem() {
   return (
     <Container>
      <ContentBlock>
+      <Title>Problema das N-rainhas - Backtracking</Title>
+      <SubTitle>Solução para o problema das N-rainhas utilizando backtracking</SubTitle>
+      <TitleMediumBackward>Descrição do problema</TitleMediumBackward>
+      <TitleShort>Problema das N-rainhas</TitleShort>
+
       <TextParagraph>
-        Sessão ainda em desenvolvimento...
+      O Problema das N-Rainhas é um problema clássico de combinação e teoria dos jogos que tem origem no xadrez. Ele pode ser descrito da seguinte forma:
       </TextParagraph>
+      <TextParagraph>
+      Dado um tabuleiro de xadrez de dimensões NxN, o objetivo é posicionar N rainhas no tabuleiro de tal forma que nenhuma rainha ataque outra. No xadrez, uma rainha pode se mover qualquer número de casas na horizontal, vertical ou diagonal. Portanto, para resolver o problema, nenhuma rainha deve compartilhar a mesma linha, coluna ou diagonal com outra rainha.
+      </TextParagraph>
+      <TextParagraph>
+      Matematicamente, o problema pode ser representado por uma matriz NxN, onde cada posição na matriz pode conter uma rainha ou estar vazia. A solução ideal é uma configuração onde todas as N rainhas estão posicionadas no tabuleiro sem conflitos.
+      </TextParagraph>
+      <TextParagraph>
+      O problema das N-Rainhas é um problema NP-completo, o que significa que a complexidade do problema cresce rapidamente com o aumento de N. Devido a essa complexidade, várias abordagens algorítmicas são utilizadas para encontrar soluções, como backtracking, algoritmos genéticos, busca tabu e outras heurísticas.
+      </TextParagraph>
+      <TextParagraph>
+      No contexto do meu desenvolvimento, implementei um algoritmo para resolver o problema das N-Rainhas, que utiliza uma abordagem específica para explorar o espaço de soluções e encontrar uma configuração válida onde todas as rainhas estão posicionadas de forma segura no tabuleiro.
+      </TextParagraph>
+
+      <TitleMediumBackward>Adaptação para esse problema em específico</TitleMediumBackward>
+
+      <TitleShort>Os Três Porquinhos e Seus Amigos</TitleShort>
+
+      <TextParagraph>
+      É sábado e os três porquinhos resolveram receber alguns amigos (no caso, cinco galinhas) para uma tarde de comilança e descontração. Eles tinham terminado de almoçar quando uma das galinhas sugeriu uma brincadeira ao ver o piso de lajotas pretas e brancas:
+      </TextParagraph>
+      <ul>
+
+        <li>O plano é colocar os porquinhos e as galinhas, um em cada lajota do piso;</li>
+        <li>O piso é quadrado e tem n × n lajotas;</li>
+        <li>Todos os porquinhos e galinhas devem fazer parte do jogo;</li>
+        <li>Um bicho só poderá ver os bichos que estão na mesma linha, coluna ou diagonais;</li>
+        <li>Um bicho só deve ver bichos da mesma espécie que ele.</li>
+      
+      </ul>
+      
+      <TextParagraph>
+      Você deve escrever um algoritmo baseado em backtracking que leia a configuração do jogo via linha de comando (como os casos que estão ao lado) e escreva como saída um inteiro para cada caso, informando quantas maneiras possíveis existem de posicionar os bichinhos. A entrada tem o seguinte formato:
+      </TextParagraph>
+
+      <ul>
+        <li>O número n de lajotas dando as dimensões do piso;</li>
+        <li>O número p de porquinhos;</li>
+        <li>O número g de galinhas;</li>
+      </ul>
+
+      <TitleMediumBackward>Como executar o código</TitleMediumBackward>
+
+      <TextParagraph>
+      A execução deste algoritmo é feita basicamente de duas maneiras, você deve compilar o arquivo Java com
+      </TextParagraph>
+
+      <Code>javac bt.java</Code>
+
+      <TextParagraph>
+      Depois, faça
+      </TextParagraph>
+
+      <ol>
+        <li>Executar testes predefinidos:</li>
+        <Code>{"java bt test"}</Code>
+
+        <li>Executar uma entrada específica:</li>
+        <Code>{"java bt <num_tile> <num_pigs> <num_chickens>"}</Code>
+      </ol>
+
      </ContentBlock>
     </Container>
   );
